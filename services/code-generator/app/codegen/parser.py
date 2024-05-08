7c7cfda550af803f78f50d3b443a2828056318bf
Colm Brandon <colm.brandon@ul.ie>
2024-05-08 22:47:03 +0100

    updated grammar to account for properties, currently omitted from parser;

--- services/code-generator/app/codegen/parser.py
+++ services/code-generator/app/codegen/parser.py
@@ -89,6 +89,12 @@ class HippoFlowTransformer(Transformer):
     def labelproperty(self, items):
         return {"label": str(items[0])[1:-1]}  # Remove quotes
 
+    def parameterproperty(self, items):
+        # to be implemented - currently returns empty list
+        if len(items) == 0:
+            return {"parameter" : []}
+        return {"parameter": []}
+
     def porttypenameproperty(self, items):
         return {"typeName": str(items[0])[1:-1]}
 
@@ -141,42 +147,44 @@ class HippoFlowParser:
     
     @classmethod
     def _get_grammar(cls) -> str:
-        grammar = """
-        hippoflow: "HippoFlow" identifier "{" (tasksib | servicesib)+ "}"
-        tasksib: "TaskSIB" identifier "at" _COORDINATE "size" _COORDINATE [index] "{" property* ports "}"
-        servicesib: "ServiceSIB" identifier "at" _COORDINATE "size" _COORDINATE [index] "{" property* ports "}"
-        property: uidproperty~1 nameproperty~1 labelproperty~1
-        ports: inputport* siblabel~1 outputport* controlflow*
+        # grammar = """
+        # hippoflow: "HippoFlow" identifier "{" (tasksib | servicesib)+ "}"
+        # tasksib: "TaskSIB" identifier "at" _COORDINATE "size" _COORDINATE [index] "{" property* ports "}"
+        # servicesib: "ServiceSIB" identifier "at" _COORDINATE "size" _COORDINATE [index] "{" property* ports "}"
+        # property: uidproperty~1 nameproperty~1 labelproperty~1
+        # ports: inputport* siblabel~1 outputport* controlflow*
 
-        uidproperty: "libraryComponentUID" ESCAPED_STRING
-        nameproperty: "name" ESCAPED_STRING
-        labelproperty: "label" ESCAPED_STRING
-        inputport: "InputPort" identifier "at" _COORDINATE "size" _COORDINATE "{" portproperties "}"
-        portproperties: porttypenameproperty~1 portnameproperty~1 portislistproperty~1
-        siblabel: "SIBLabel" identifier "at" _COORDINATE "size" _COORDINATE "{" "label" ESCAPED_STRING "}"
-        outputport: "OutputPort" identifier "at" _COORDINATE "size" _COORDINATE "{" portproperties dataflow* "}"
-        controlflow: "-ControlFlow->" identifier "decorate" ESCAPED_STRING "at" _positionpair "decorate" ESCAPED_STRING "at" _positionpair "{" cfproperties "}"
+        # uidproperty: "libraryComponentUID" ESCAPED_STRING
+        # nameproperty: "name" ESCAPED_STRING
+        # labelproperty: "label" ESCAPED_STRING
+        # inputport: "InputPort" identifier "at" _COORDINATE "size" _COORDINATE "{" portproperties "}"
+        # portproperties: porttypenameproperty~1 portnameproperty~1 portislistproperty~1
+        # siblabel: "SIBLabel" identifier "at" _COORDINATE "size" _COORDINATE "{" "label" ESCAPED_STRING "}"
+        # outputport: "OutputPort" identifier "at" _COORDINATE "size" _COORDINATE "{" portproperties dataflow* "}"
+        # controlflow: "-ControlFlow->" identifier "decorate" ESCAPED_STRING "at" _positionpair "decorate" ESCAPED_STRING "at" _positionpair "{" cfproperties "}"
 
-        porttypenameproperty: "typeName" ESCAPED_STRING
-        portnameproperty: "name" ESCAPED_STRING
-        portislistproperty: "isList" BOOLEAN
-        dataflow: "-DataFlow->" identifier "via" _positionpairs "{" dfid~1 "}"
-        dfid: "id" identifier
-        cfproperties: cfid~1 cflabel~1
-        cfid: "id" identifier
-        cflabel: "label" ESCAPED_STRING
+        # porttypenameproperty: "typeName" ESCAPED_STRING
+        # portnameproperty: "name" ESCAPED_STRING
+        # portislistproperty: "isList" BOOLEAN
+        # dataflow: "-DataFlow->" identifier "via" _positionpairs "{" dfid~1 "}"
+        # dfid: "id" identifier
+        # cfproperties: cfid~1 cflabel~1
+        # cfid: "id" identifier
+        # cflabel: "label" ESCAPED_STRING
 
-        _positionpair: "(" _COORDINATE ")"
-        _positionpairs: _positionpair+
-        identifier: /_[a-zA-Z0-9\-\_]+/
-        _COORDINATE: /\-*\d+,\-*\d+/
-        index: "index" /\d+/
-        BOOLEAN: "true" | "false"
+        # _positionpair: "(" _COORDINATE ")"
+        # _positionpairs: _positionpair+
+        # identifier: /_[a-zA-Z0-9\-\_]+/
+        # _COORDINATE: /\-*\d+,\-*\d+/
+        # index: "index" /\d+/
+        # BOOLEAN: "true" | "false"
 
-        %import common.ESCAPED_STRING
-        %import common.WS
-        %ignore WS
-        """
+        # %import common.ESCAPED_STRING
+        # %import common.WS
+        # %ignore WS
+        # """
+        with open('./templates/grammar.lark', 'r') as file:
+            grammar = file.read()
         return grammar
     
     @classmethod
