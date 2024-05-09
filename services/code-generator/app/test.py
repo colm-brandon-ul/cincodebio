8d8847b546b6749bc6b4bc1a7a56a4e077b4152e
Colm Brandon <colm.brandon@ul.ie>
2024-05-09 12:52:53 +0100

    correcting accidental deletion in code-gen template

--- services/code-generator/app/test.py
+++ services/code-generator/app/test.py
@@ -1,110 +1,78 @@
-sib_map = {'inputs': {'AceWSI': {'whole_slide_image:WholeSlideImage': 'Data'}, 'CropCoresTMA': {'tissue_micro_array:TissueMicroArray': 'Data', 'rois:ROIs': 'WorkflowParameters'}, 'SegArrayTMA': {'tissue_micro_array:TissueMicroArray': 'Data', 'nuclear_stain:NuclearStain': 'WorkflowParameters'}, 'CreateMissileObjectDTMA': {'dearrayed_tissue_micro_array_missile_fcs:DearrayedTissueMicroArrayMissileFCS': 'Data', 'protein_channel_markers:ProteinChannelMarkers': 'WorkflowParameters'}, 'AceDTMA': {'dearrayed_tissue_micro_array:DearrayedTissueMicroArray': 'Data'}, 'CreateMissileObjectWSI': {'whole_slide_image_missile_fcs:WholeSlideImageMissileFCS': 'Data', 'protein_channel_markers:ProteinChannelMarkers': 'WorkflowParameters'}, 'InitWSI': {}, 'EditPredictedRoisTMA': {'tissue_micro_array:TissueMicroArray': 'Data', 'nuclear_stain:NuclearStain': 'WorkflowParameters', 'predicted_rois:PredictedROIs': 'WorkflowParameters'}, 'ManualDearrayTMA': {'tissue_micro_array:TissueMicroArray': 'Data', 'nuclear_stain:NuclearStain': 'WorkflowParameters'}, 'InitTMA': {}}, 
-'outputs': {'AceWSI': {'whole_slide_image:WholeSlideImage': 'Data'}, 'CropCoresTMA': {'dearrayed_tissue_micro_array:DearrayedTissueMicroArray': 'Data'}, 'SegArrayTMA': {'predicted_rois:PredictedROIs': 'WorkflowParameters'}, 'CreateMissileObjectDTMA': {'missile_metadata:MissileMetadata': 'Data', 'missile_counts:MissileExpressionCounts': 'Data', 'missile_spatial_data:MissileExpressionSpatialData': 'Data'}, 'AceDTMA': {'dearrayed_tissue_micro_array:DearrayedTissueMicroArray': 'Data'}, 'CreateMissileObjectWSI': {'missile_metadata:MissileMetadata': 'Data', 'missile_counts:MissileExpressionCounts': 'Data', 'missile_spatial_data:MissileExpressionSpatialData': 'Data'}, 'InitWSI': {'whole_slide_image:WholeSlideImage': 'Data', 'nuclear_stain:NuclearStain': 'WorkflowParameters', 'nuclear_markers:NuclearMarkers': 'WorkflowParameters', 'membrane_markers:MembraneMarkers': 'WorkflowParameters', 'protein_channel_markers:ProteinChannelMarkers': 'WorkflowParameters'}, 'EditPredictedRoisTMA': {'rois:ROIs': 'WorkflowParameters'}, 'ManualDearrayTMA': {'rois:ROIs': 'WorkflowParameters'}, 'InitTMA': {'tissue_micro_array:TissueMicroArray': 'Data', 'nuclear_stain:NuclearStain': 'WorkflowParameters', 'nuclear_markers:NuclearMarkers': 'WorkflowParameters', 'membrane_markers:MembraneMarkers': 'WorkflowParameters', 'protein_channel_markers:ProteinChannelMarkers': 'WorkflowParameters'}}, 
-'concepts': {'AceWSI': 'TechnicalVarianceCorrection', 'CropCoresTMA': 'DeArray', 'SegArrayTMA': 'DeArray', 'CreateMissileObjectDTMA': 'DataTransformation', 'AceDTMA': 'TechnicalVarianceCorrection', 'CreateMissileObjectWSI': 'DataTransformation', 'InitWSI': 'Start', 'EditPredictedRoisTMA': 'DeArray', 'ManualDearrayTMA': 'DeArray', 'InitTMA': 'Start'}}
+sib_map = {'concepts': {'AceWSI': 'TechnicalVarianceCorrection', 'CropCoresTMA': 'DeArray', 'SegArrayTMA': 'DeArray', 'CreateMissileObjectDTMA': 'DataTransformation', 'AceDTMA': 'TechnicalVarianceCorrection', 'CreateMissileObjectWSI': 'DataTransformation', 'InitWSI': 'Start', 'EditPredictedRoisTMA': 'DeArray', 'ManualDearrayTMA': 'DeArray', 'InitTMA': 'Start'}, 'inputs': {'AceWSI': {'whole_slide_image:WholeSlideImage': 'Data'}, 'CropCoresTMA': {'tissue_micro_array:TissueMicroArray': 'Data', 'rois:ROIs': 'WorkflowParameters'}, 'SegArrayTMA': {'tissue_micro_array:TissueMicroArray': 'Data', 'nuclear_stain:NuclearStain': 'WorkflowParameters'}, 'CreateMissileObjectDTMA': {'dearrayed_tissue_micro_array_missile_fcs:DearrayedTissueMicroArrayMissileFCS': 'Data', 'protein_channel_markers:ProteinChannelMarkers': 'WorkflowParameters'}, 'AceDTMA': {'dearrayed_tissue_micro_array:DearrayedTissueMicroArray': 'Data'}, 'CreateMissileObjectWSI': {'whole_slide_image_missile_fcs:WholeSlideImageMissileFCS': 'Data', 'protein_channel_markers:ProteinChannelMarkers': 'WorkflowParameters'}, 'InitWSI': {}, 'EditPredictedRoisTMA': {'tissue_micro_array:TissueMicroArray': 'Data', 'nuclear_stain:NuclearStain': 'WorkflowParameters', 'predicted_rois:PredictedROIs': 'WorkflowParameters'}, 'ManualDearrayTMA': {'tissue_micro_array:TissueMicroArray': 'Data', 'nuclear_stain:NuclearStain': 'WorkflowParameters'}, 'InitTMA': {}}, 'outputs': {'AceWSI': {'whole_slide_image:WholeSlideImage': 'Data'}, 'CropCoresTMA': {'dearrayed_tissue_micro_array:DearrayedTissueMicroArray': 'Data'}, 'SegArrayTMA': {'predicted_rois:PredictedROIs': 'WorkflowParameters'}, 'CreateMissileObjectDTMA': {'missile_metadata:MissileMetadata': 'Data', 'missile_counts:MissileExpressionCounts': 'Data', 'missile_spatial_data:MissileExpressionSpatialData': 'Data'}, 'AceDTMA': {'dearrayed_tissue_micro_array:DearrayedTissueMicroArray': 'Data'}, 'CreateMissileObjectWSI': {'missile_metadata:MissileMetadata': 'Data', 'missile_counts:MissileExpressionCounts': 'Data', 'missile_spatial_data:MissileExpressionSpatialData': 'Data'}, 'InitWSI': {'whole_slide_image:WholeSlideImage': 'Data', 'nuclear_stain:NuclearStain': 'WorkflowParameters', 'nuclear_markers:NuclearMarkers': 'WorkflowParameters', 'membrane_markers:MembraneMarkers': 'WorkflowParameters', 'protein_channel_markers:ProteinChannelMarkers': 'WorkflowParameters'}, 'EditPredictedRoisTMA': {'rois:ROIs': 'WorkflowParameters'}, 'ManualDearrayTMA': {'rois:ROIs': 'WorkflowParameters'}, 'InitTMA': {'tissue_micro_array:TissueMicroArray': 'Data', 'nuclear_stain:NuclearStain': 'WorkflowParameters', 'nuclear_markers:NuclearMarkers': 'WorkflowParameters', 'membrane_markers:MembraneMarkers': 'WorkflowParameters', 'protein_channel_markers:ProteinChannelMarkers': 'WorkflowParameters'}}}
 
-test_wf = """HippoFlow _Nm3M0P5IEe6GW8J3C84YnA {
-  TaskSIB _Zop_IQGHEe-VG6OBafcrYg at 250,70 size 240,220 {
-  	libraryComponentUID "_71fc05b6_c395_4144_a78f_53ceaed17f92"
+test_wf = """HippoFlow _JCx7AA1ZEe-bt9bPK9Z6oQ {
+  TaskSIB _ev7SwQ3vEe-bt9bPK9Z6oQ at 60,180 size 240,220 {
+  	libraryComponentUID "_b0088fee_75aa_4965_9586_d1da0be1a14a"
   	name "InitTMA"
   	label "InitTMA"
-  	SIBLabel _ZotpgQGHEe-VG6OBafcrYg at 2,10 size 236,50 {
+  	parameter [  ]
+  	SIBLabel _ev-WEQ3vEe-bt9bPK9Z6oQ at 2,10 size 236,50 {
   		label "InitTMA"
   	}
-  	OutputPort _Zou3oQGHEe-VG6OBafcrYg at 2,60 size 236,30 {
+  	
+  	OutputPort _ewAyUQ3vEe-bt9bPK9Z6oQ at 2,60 size 236,30 {
   		typeName "TissueMicroArray"
   		name "tissue_micro_array"
   		isList false
-  		-DataFlow-> _cFOvQQGHEe-VG6OBafcrYg via (200,145) (200,415)  {
-  			id _cqbHAQGHEe-VG6OBafcrYg
-  		}
-  		-DataFlow-> _gDgisQGHEe-VG6OBafcrYg via (170,145) (170,655)  {
-  			id _ks8zAQGHEe-VG6OBafcrYg
+  		-DataFlow-> _e7g3sQ3vEe-bt9bPK9Z6oQ via (40,255) (40,225)  {
+  			id _gbQMIQ3vEe-bt9bPK9Z6oQ
   		}
   	}
-  	OutputPort _ZowFwQGHEe-VG6OBafcrYg at 2,90 size 236,30 {
+  	
+  	OutputPort _ewCngQ3vEe-bt9bPK9Z6oQ at 2,90 size 236,30 {
   		typeName "NuclearStain"
   		name "nuclear_stain"
   		isList false
-  		-DataFlow-> _cFQkcQGHEe-VG6OBafcrYg via (180,175) (180,445)  {
-  			id _dAUegQGHEe-VG6OBafcrYg
-  		}
-  		-DataFlow-> _gDhw0QGHEe-VG6OBafcrYg via (150,175) (150,685)  {
-  			id _lVidAQGHEe-VG6OBafcrYg
+  		-DataFlow-> _e7kiEQ3vEe-bt9bPK9Z6oQ via (20,285) (20,255)  {
+  			id _g0KbYQ3vEe-bt9bPK9Z6oQ
   		}
   	}
-  	OutputPort _ZoxT4QGHEe-VG6OBafcrYg at 2,120 size 236,30 {
+  	
+  	OutputPort _ewIHEQ3vEe-bt9bPK9Z6oQ at 2,120 size 236,30 {
   		typeName "NuclearMarkers"
   		name "nuclear_markers"
   		isList false
   	}
-  	OutputPort _ZozJEQGHEe-VG6OBafcrYg at 2,150 size 236,30 {
+  	
+  	OutputPort _ewLKYQ3vEe-bt9bPK9Z6oQ at 2,150 size 236,30 {
   		typeName "MembraneMarkers"
   		name "membrane_markers"
   		isList false
   	}
-  	OutputPort _Zo1lUQGHEe-VG6OBafcrYg at 2,180 size 236,30 {
+  	
+  	OutputPort _ewMYgQ3vEe-bt9bPK9Z6oQ at 2,180 size 236,30 {
   		typeName "ProteinChannelMarkers"
   		name "protein_channel_markers"
   		isList false
   	}
-  	-ControlFlow-> _cFM6EQGHEe-VG6OBafcrYg  decorate "" at (0,0) decorate "Text" at (0,0) {
-  		id _93d0EQIeEe-VG6OBafcrYg
+  	-ControlFlow-> _e7dNUQ3vEe-bt9bPK9Z6oQ  decorate "" at (0,0) decorate "Text" at (0,0) {
+  		id _fpRVkQ3vEe-bt9bPK9Z6oQ
   		label "success"
   	}
   }
-  ServiceSIB _cFM6EQGHEe-VG6OBafcrYg at 220,390 size 240,160 {
-  	libraryComponentUID "_f3f5c104_4879_47a0_af99_499d2eabf8fa"
-  	name "SegArrayTMA"
-  	label "SegArrayTMA"
-  	InputPort _cFOvQQGHEe-VG6OBafcrYg at 2,10 size 236,30 {
+  
+  TaskSIB _e7dNUQ3vEe-bt9bPK9Z6oQ at 430,200 size 240,160 {
+  	libraryComponentUID "_57f1386a_1ffb_4739_8872_42caa30d865c"
+  	name "ManualDearrayTMA"
+  	label "ManualDearrayTMA"
+  	parameter [  ]
+  	InputPort _e7g3sQ3vEe-bt9bPK9Z6oQ at 2,10 size 236,30 {
   		typeName "TissueMicroArray"
   		name "tissue_micro_array"
   		isList false
   	}
-  	InputPort _cFQkcQGHEe-VG6OBafcrYg at 2,40 size 236,30 {
+  	
+  	InputPort _e7kiEQ3vEe-bt9bPK9Z6oQ at 2,40 size 236,30 {
   		typeName "NuclearStain"
   		name "nuclear_stain"
   		isList false
   	}
-  	SIBLabel _cFSZoQGHEe-VG6OBafcrYg at 2,70 size 236,50 {
-  		label "SegArrayTMA"
+  	
+  	SIBLabel _e7m-UQ3vEe-bt9bPK9Z6oQ at 2,70 size 236,50 {
+  		label "ManualDearrayTMA"
   	}
-  	OutputPort _cFSZowGHEe-VG6OBafcrYg at 2,120 size 236,30 {
-  		typeName "PredictedROIs"
-  		name "predicted_rois"
-  		isList false
-  		-DataFlow-> _gDjmAQGHEe-VG6OBafcrYg via (190,525) (190,715)  {
-  			id _hR6PgQGHEe-VG6OBafcrYg
-  		}
-  	}
-  	-ControlFlow-> _gDetgQGHEe-VG6OBafcrYg  decorate "" at (0,0) decorate "Text" at (0,0) {
-  		id _-qIAEQIeEe-VG6OBafcrYg
-  		label "success"
-  	}
-  }
-  TaskSIB _gDetgQGHEe-VG6OBafcrYg at 220,630 size 240,190 {
-  	libraryComponentUID "_647a4bc7_574c_4420_a43d_bde1e38dbf49"
-  	name "EditPredictedRoisTMA"
-  	label "EditPredictedRoisTMA"
-  	InputPort _gDgisQGHEe-VG6OBafcrYg at 2,10 size 236,30 {
-  		typeName "TissueMicroArray"
-  		name "tissue_micro_array"
-  		isList false
-  	}
-  	InputPort _gDhw0QGHEe-VG6OBafcrYg at 2,40 size 236,30 {
-  		typeName "NuclearStain"
-  		name "nuclear_stain"
-  		isList false
-  	}
-  	InputPort _gDjmAQGHEe-VG6OBafcrYg at 2,70 size 236,30 {
-  		typeName "PredictedROIs"
-  		name "predicted_rois"
-  		isList false
-  	}
-  	SIBLabel _gDk0IQGHEe-VG6OBafcrYg at 2,100 size 236,50 {
-  		label "EditPredictedRoisTMA"
-  	}
-  	OutputPort _gDmCQQGHEe-VG6OBafcrYg at 2,150 size 236,30 {
+  	
+  	OutputPort _e7oMcQ3vEe-bt9bPK9Z6oQ at 2,120 size 236,30 {
   		typeName "ROIs"
   		name "rois"
   		isList false
