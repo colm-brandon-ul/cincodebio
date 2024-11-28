6eefd364e4929ded80f5b412a277b52a8bf5b48c
Colm Brandon <colm.brandon@ul.ie>
2024-11-28 00:45:13 +0000

    fixed execution env redirect to match refactored frontend

--- services/sib-manager/app/src/static-code/utils.py
+++ services/sib-manager/app/src/static-code/utils.py
@@ -1,2 +1,2 @@
 def create_redirect_to_execution_env_link(job_id: str, base_url: str, execution_ingress_path: str):
-    return f"{str(base_url)}{execution_ingress_path}/frontend/{job_id}"
+    return f"{str(base_url)}workflows/{job_id}"
