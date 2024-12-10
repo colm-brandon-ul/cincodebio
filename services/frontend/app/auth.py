c894d22b327bfe015c3b3d8ea8598dcbf829b168
Colm Brandon <colm.brandon@ul.ie>
2024-12-10 19:41:04 +0000

    added logging for debug

--- services/frontend/app/auth.py
+++ services/frontend/app/auth.py
@@ -1,6 +1,7 @@
 # from fastapi import FastAPI, Request
 # from fastapi.responses import RedirectResponse
 
+import logging
 from typing import Optional
 from fastapi import Depends, HTTPException, Security
 from fastapi.security import OAuth2PasswordBearer
@@ -24,25 +25,30 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 
 def decode_token(token: str):
     from config import AUTH_PUBLIC_KEY as PUBLIC_KEY
+    logging.warning(PUBLIC_KEY)
     try:
         payload = jwt.decode(token, PUBLIC_KEY, algorithms=['RS256'])
         return payload
     except jwt.PyJWTError as e:
+        logging.warning(e)
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"Could not validate credentials: {e}"
         )
     except jwt.InvalidSignatureError as eise:
+        logging.warning(eise)
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"Invalid token signature: {eise}"
         )
     except jwt.ExpiredSignatureError as eese:
+        logging.warning(eese)
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"Token has expired: {eese}"
         )
     except jwt.InvalidTokenError as eite:
+        logging.warning(eite)
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"Invalid token: {eite}"
@@ -54,7 +60,6 @@ async def validate_token(credentials: HTTPAuthorizationCredentials = Security(se
     Validate JWT token from Authorization header
     """
     try:
-
         import logging
         logging.warning(credentials)
         token = credentials.credentials
